0a32076468eaaa78a4462c4e12c63aa6b71ed2e5
Vignesh250320 <omvicky333@gmail.com>
2025-11-08 16:05:17 +0530

    Remove test files and temporary guides from repository

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("DB Host:", os.getenv("DB_HOST"))
-print("API Base:", os.getenv("CRICBUZZ_API_BASE"))
-print("API Key:", os.getenv("RAPIDAPI_KEY")[:6] + "********")  # partially hide
